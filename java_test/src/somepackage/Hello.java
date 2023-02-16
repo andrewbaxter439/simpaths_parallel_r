6323d131429eabc8f732ad8bfc886bf8db06c458
Andy Baxter <Andy.Baxter@glasgow.ac.uk>
2023-02-16 16:29:55 +0000

    making tidier

--- java_test/src/somepackage/Hello.java
+++ java_test/src/somepackage/Hello.java
@@ -3,9 +3,12 @@ package somepackage;
 public class Hello {
     public static void main(String[] args) {
     System.out.println("Hello, world! From Java :X");
+    Calculator calc = new Calculator();
+    calc.addUp(new int[]{1, 2, 3});
     SayHi(args);
+
     }
-    
+
     public static void SayHi(String[] args) {
     System.out.println("Hello " +  args[0]+ "!");
     }
