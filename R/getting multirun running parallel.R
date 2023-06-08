4dd8432d1649194c9bfe72e26abf39cdbbc6b21c
Andrew Baxter <andrewbaxter439@gmail.com>
2023-06-08 15:57:59 +0100

    multirun file running smoothly

--- R/getting multirun running parallel.R
+++ R/getting multirun running parallel.R
@@ -1,15 +1,14 @@
-class_paths <- "model.jar"
+class_paths <- "labsim.jar"
 
 # test multi-run ----------------------------------------------------------
-class_path <- "labsim.jar"
 
 library(doParallel)
 cores <- parallel::detectCores()
 
-clusters <- 6
-n_runs <- 10
+clusters <- 20
+n_runs <- 20
 
-cl <- parallel::makePSOCKcluster(6)
+cl <- parallel::makePSOCKcluster(clusters)
 
 registerDoParallel(cl)
 
@@ -17,27 +16,18 @@ seed_starts <- purrr::map_int(seq(1, clusters), ~ .x * 100)
 staggered_starts <- purrr::map_int(seq(1, clusters), ~ .x * 20 - 20)
 
 
-
-# class_paths <- vapply(
-#   c("labsim/target/model-1.0.0.jar", 
-#     "labsim/target/JAS-mine-core-4.1.0-jar-with-dependencies.jar",
-#     dir("labsim/target/dependency", recursive = TRUE, full.names = TRUE)),
-#   \(path) file.path(getwd(), path),
-#   character(1),
-#   USE.NAMES = FALSE
-# )
-
 t1 <- Sys.time()
 foreach(seed = seed_starts, wait = staggered_starts, n_runs = rep(n_runs, clusters), .packages = "rJava", .verbose = TRUE) %dopar% {
   Sys.sleep(wait)
   .jinit()
-  .jaddClassPath(class_path)
+  .jaddClassPath("labsim.jar")
   SimPathsMultiRun <- .jnew("simpaths.experiment.SimPathsMultiRun")
   .jcall(SimPathsMultiRun, method = "main", , c("-r", as.character(seed),
                                                 "-g", "false",
                                                 "-n", as.character(n_runs), 
                                                 "-s", "2017",
-                                                "-p", "10000",
+                                                "-e", "2025",
+                                                "-p", "75000",
                                                 "-f"
                                                 ))
 }
@@ -49,8 +39,10 @@ cat("This took", signif(difftime, 2), attr(difftime, "units"))
 library(rJava)
 .jinit()
 
+class_paths <- "labsim.jar"
+
 # .jaddClassPath("labsim/target/model-1.0.0-jar-with-dependencies.jar")
-.jaddClassPath(class_path)
+.jaddClassPath(class_paths)
 
 .jclassPath()
 
@@ -62,3 +54,12 @@ SimPathsMultiRun <- .jnew("simpaths.experiment.SimPathsMultiRun")
 .jcall(SimPathsMultiRun, method = "main", , c("-r", "502", "-g", "false", "-n", "1", "-f"))
 .jcall(SimPathsMultiRun, method = "main", , c("-r", "503", "-g", "false", "-n", "1", "-f"))
 .jcall(SimPathsMultiRun, method = "main", , c("-r", "504", "-g", "false", "-n", "1", "-f"))
+
+# class_paths <- vapply(
+#   c("labsim/target/model-1.0.0.jar", 
+#     "labsim/target/JAS-mine-core-4.1.0-jar-with-dependencies.jar",
+#     dir("labsim/target/dependency", recursive = TRUE, full.names = TRUE)),
+#   \(path) file.path(getwd(), path),
+#   character(1),
+#   USE.NAMES = FALSE
+# )
