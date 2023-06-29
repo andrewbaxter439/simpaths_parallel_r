3212580428e55ecd9bd00bd0536ff1821ac611a4
Andrew Baxter <andrewbaxter439@gmail.com>
2023-06-29 17:43:36 +0100

    local side fix - to be overwritten by machine side

--- R/getting multirun running parallel.R
+++ R/getting multirun running parallel.R
@@ -41,12 +41,11 @@ library(rJava)
 
 class_paths <- "labsim.jar"
 
-# .jaddClassPath("labsim/target/model-1.0.0-jar-with-dependencies.jar")
 .jaddClassPath(class_paths)
 
 .jclassPath()
-
 SimPathsMultiRun <- .jnew("simpaths.experiment.SimPathsMultiRun")
+
 
 .jcall(SimPathsMultiRun, method = "main", , c("-r", "301", "-g", "false", "-n", "1", "-p", "75000", "-s", "2017", "-e", "2022", "-f"))
 .jcall(SimPathsMultiRun, method = "main", , c("-r", "300", "-g", "false", "-n", "10", "-f"))
