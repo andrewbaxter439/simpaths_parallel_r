82a2e13c806206aafe676d91933d174e0f507a10
Andy Baxter <Andy.Baxter@glasgow.ac.uk>
2023-06-29 17:38:23 +0100

    making things work on remote machine

--- R/getting multirun running parallel.R
+++ R/getting multirun running parallel.R
@@ -1,12 +1,12 @@
-class_paths <- "labsim.jar"
+class_paths <- "simpaths.jar"
 
 # test multi-run ----------------------------------------------------------
 
 library(doParallel)
 cores <- parallel::detectCores()
 
-clusters <- 20
-n_runs <- 20
+clusters <- 50 
+n_runs <- 20 
 
 cl <- parallel::makePSOCKcluster(clusters)
 
@@ -17,10 +17,10 @@ staggered_starts <- purrr::map_int(seq(1, clusters), ~ .x * 20 - 20)
 
 
 t1 <- Sys.time()
-foreach(seed = seed_starts, wait = staggered_starts, n_runs = rep(n_runs, clusters), .packages = "rJava", .verbose = TRUE) %dopar% {
+foreach(seed = seed_starts, wait = staggered_starts, .packages = "rJava", .verbose = TRUE) %dopar% {
   Sys.sleep(wait)
   .jinit()
-  .jaddClassPath("labsim.jar")
+  .jaddClassPath(class_paths)
   SimPathsMultiRun <- .jnew("simpaths.experiment.SimPathsMultiRun")
   .jcall(SimPathsMultiRun, method = "main", , c("-r", as.character(seed),
                                                 "-g", "false",
@@ -36,30 +36,3 @@ cat("This took", signif(difftime, 2), attr(difftime, "units"))
 
 # test one run ------------------------------------------------------------
 
-library(rJava)
-.jinit()
-
-class_paths <- "labsim.jar"
-
-# .jaddClassPath("labsim/target/model-1.0.0-jar-with-dependencies.jar")
-.jaddClassPath(class_paths)
-
-.jclassPath()
-
-SimPathsMultiRun <- .jnew("simpaths.experiment.SimPathsMultiRun")
-
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "301", "-g", "false", "-n", "1", "-p", "75000", "-s", "2017", "-e", "2022", "-f"))
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "300", "-g", "false", "-n", "10", "-f"))
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "400", "-g", "false", "-n", "10", "-f"))
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "502", "-g", "false", "-n", "1", "-f"))
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "503", "-g", "false", "-n", "1", "-f"))
-.jcall(SimPathsMultiRun, method = "main", , c("-r", "504", "-g", "false", "-n", "1", "-f"))
-
-# class_paths <- vapply(
-#   c("labsim/target/model-1.0.0.jar", 
-#     "labsim/target/JAS-mine-core-4.1.0-jar-with-dependencies.jar",
-#     dir("labsim/target/dependency", recursive = TRUE, full.names = TRUE)),
-#   \(path) file.path(getwd(), path),
-#   character(1),
-#   USE.NAMES = FALSE
-# )
